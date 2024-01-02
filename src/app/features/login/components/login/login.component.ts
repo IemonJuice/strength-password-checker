94ad3b90f07429bb6aba0979e5797212e6a7f234
Yurchenko Pavlo <yurchenkopavlo3@gmail.com>
2024-01-02 14:15:12 +0200

    added feature password checker

--- src/app/features/login/components/login/login.component.ts
+++ src/app/features/login/components/login/login.component.ts
@@ -8,13 +8,28 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class LoginComponent {
   form: FormGroup = inject(FormBuilder).group({
-    username:['',[Validators.required]],
-    password:['',[Validators.required]],
+    username: [''],
+    password: ['', [Validators.min(8)]],
   });
+
+  isEasyPassword() {
+    const easyPassword = /^[a-zA-Z]+$|^\d+$|^[^a-zA-Z\d\s]+$/;
+    return easyPassword.test(this.form.getRawValue().password);
+  }
+
+  isMediumPassword() {
+    const middlePassword = /^(?=.*[a-zA-Z])(?=.*\d)|(?=.*[a-zA-Z])(?=.*[^a-zA-Z\d\s])|(?=.*\d)(?=.*[^a-zA-Z\d\s])/;
+    return middlePassword.test(this.form.getRawValue().password);
+  }
+
+  isStrongPassword() {
+    const strongPassword = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[^a-zA-Z\d\s])/;
+    return strongPassword.test(this.form.getRawValue().password);
+  }
 
   // imitation of the backend
   submitForm() {
-    if(this.form.valid){
+    if (this.form.valid) {
       console.log(this.form.getRawValue())
     }
   }
