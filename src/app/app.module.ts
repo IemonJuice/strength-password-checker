c9d81c9ccb7ea086587f81f1eb52ffe2719fd8c4
Yurchenko Pavlo <yurchenkopavlo3@gmail.com>
2024-01-02 15:02:26 +0200

    fix typo routing folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {AppRoutingModule} from "./core/rooting/app-routing.module";
+import {AppRoutingModule} from "./core/routing/app-routing.module";
 
 @NgModule({
   declarations: [
